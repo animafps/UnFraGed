ecf571c9da992f70ea8cab65451b822ebdecce4e
lukecsnz <72008@selwyn.school.nz>
2020-05-05 16:50:46 +1200

    Added Test World and timer and crosshair

---
+++ Assets/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool finished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        startTime = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(finished)
+            return;
+
+        float t = Time.time - startTime;
+
+        string minuites = ((int) t / 60).ToString();
+        string seconds = ( t % 60).ToString("f2");
+
+        timerText.text = minuites + ":" + seconds;
+    }
+
+    public void Finish()
+    {
+        finished = true;
+    }
+}
---
+++ Assets/finish.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finish : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("_Player").SendMessage("Finish");
+    }
+    
+}
